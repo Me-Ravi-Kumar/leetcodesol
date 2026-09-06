07441e8c274973dc87a06f604a84273c63798d02
Ravi Kumar <127765301+Me-Ravi-Kumar@users.noreply.github.com>
2026-09-06 22:09:06 +0530

    Refactor numDistinct function for optimization

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -26,3 +26,65 @@ public:
         return prev[n];
     }
 };
+--bottom up----
+class Solution {
+public:
+    typedef unsigned long long ull;
+    int numDistinct(string s, string t) {
+        int m = s.length();
+        int n = t.length();
+        
+        vector<vector<ull>> dp(m+1, vector<ull>(n+1));
+        
+        for(int row = 0; row<m+1; row++) {
+            dp[row][0] = 1;
+        }
+        
+        for(int col = 1; col<n+1; col++) {
+            dp[0][col] = 0;
+        }
+        
+        for(int i = 1; i<m+1; i++) {
+            for(int j = 1; j<n+1; j++) {
+                if(s[i-1] == t[j-1])
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        
+        
+        return dp[m][n];
+    }
+};
+
+--optimized--bottom--up---
+class Solution {
+public:
+    typedef unsigned long long ull;
+    
+    int numDistinct(string s, string t) {
+        int m = s.length();
+        int n = t.length();
+
+        vector<ull> curr(n+1, 0); 
+        vector<ull> prev(n+1, 0);
+        
+        prev[0] = curr[0] = 1;
+        
+        for(int i = 1; i<m+1; i++) {
+            
+            for(int j = 1; j<n+1; j++) {
+                
+                if(s[i-1] == t[j-1])
+                    curr[j] = prev[j-1] + prev[j];
+                else
+                    curr[j] = prev[j];
+            }
+            prev = curr;
+        }
+
+        return prev[n];
+    }
+};
+
